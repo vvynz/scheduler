158fa48fec68fc9eae66090f1d055f3d4018157c
Vivian Zhang <vvynz12@gmail.com>
2022-02-23 16:15:26 -0500

    Implement the InterviewerList and InterviewerListItem componenets. Include the scss files for the componenets and implus the stories for the components in the index.js file

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+
+export default function InterviewerList(props) {
+  console.log("LIST PROPS", props);
+
+  const interviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={props.interviewer === interviewer.id}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        {...interviewer}
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
+}
---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  // console.log(props);
+  const { name, avatar, selected } = props;
+  const interviewersClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected,
+  });
+
+  return (
+    <li className={interviewersClass} onClick={props.setInterviewer}>
+      <img className="interviewers__item-image" src={avatar} alt={name} />
+      {selected && name}
+    </li>
+  );
+
+  // (
+  //   <li className={interviewersClass} onClick={props.setInterviewer}>
+  //     <img className="interviewers__item-image" src={avatar} alt={name} />
+  //   </li>
+  // );
+}
