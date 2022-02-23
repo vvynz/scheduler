44aa8e790181789e59db772d4d5d5045cd940050
Vivian Zhang <vvynz12@gmail.com>
2022-02-23 17:38:06 -0500

    minor updates

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -7,7 +7,7 @@ export default function DayList(props) {
 
   // check to see if the day that is selected matches the name of the day in the object we are currently processing. 
   // the object here in .map, would be "day"
-  const days = props.days.map(day => <DayListItem key={day.id} selected={props.day === day.name} setDay={props.setDay} {...day} />);
+  const days = props.days.map(day => <DayListItem key={day.id} selected={props.value === day.name} setDay={props.onChange} {...day} />);
 
   return (
     <ul>{days}</ul>
