1739230b400c2ceb085825f3130cb39eeca6bea3
Vivian Zhang <vvynz12@gmail.com>
2022-02-24 18:25:40 -0500

    Import Header, Show, and Empty component. Pass in props to render appointment time. If there is an appointment, render student name and interviewer name, else render the Empty component

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
-  return <article className="appointment">Appointment at {props.time}</article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          // id={props.id}
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+      {/* {props.interview && (
+        <Show
+          interview={{
+            student: props.interview.student,
+            interviewer: props.interview.interviewer.name,
+          }}
+        />
+      )}
+      {!props.interview && <Empty />} */}
+    </article>
+  );
 }
