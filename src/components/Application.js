68ff291659361c2f77ed1c2a7e7fbcc5d0d9a141
Vivian Zhang <vvynz12@gmail.com>
2022-02-24 18:26:51 -0500

    Import the Appointment folder/component and render the Appointments in the schedule section.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from 'components/DayList';
+import Appointment from 'components/Appointment';
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const [days, setDays] = useState([]);
+
+  // console.log("APPTS????", appointments);
+  const apptArr = appointments.map(appointment => <Appointment key={appointment.id} {...appointment} />)
+
+  useEffect(() => {
+    axios.get('/api/days').then(response => {
+      console.log(response.data);
+      setDays([...response.data]);
+    })
+  }, [day])
 
   return (
     <main className="layout">
@@ -33,7 +68,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={days} value={day} onChange={setDays} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -42,7 +77,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {apptArr}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
