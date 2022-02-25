a8b466789ccd3a0c4ad214229037b48ab31c5b44
Vivian Zhang <vvynz12@gmail.com>
2022-02-25 17:51:43 -0500

    Export the helper function

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,7 +3,7 @@ import DayListItem from 'components/DayListItem';
 
 
 export default function DayList(props) {
-  console.log(props.days);
+  // console.log(props.days);
 
   // check to see if the day that is selected matches the name of the day in the object we are currently processing. 
   // the object here in .map, would be "day"
