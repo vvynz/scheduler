037297c3f87bed0e09a1728dbd142f775187c479
Vivian Zhang <vvynz12@gmail.com>
2022-02-25 16:53:36 -0500

    create a getAppointmentsForDay helper function to retrive an array of appointment for the given day. Include some jest tests to test the function as well

---
+++ src/helpers/selectors.js
@@ -0,0 +1,31 @@
+export function getAppointmentsForDay(state, day) {
+  //Returns an array of obj from state.days where the name matches the provided day
+  let apptArr = [];
+
+  const filteredDays = state.days.filter(date => {
+    if (date.name === day) {
+      // return date;
+
+      const stateArr = Object.values(state.appointments);
+      // console.log("OBJ???", stateArr);
+      const apptObj = stateArr.map(appt => {
+        // console.log("MEOW", appt.id);
+        // console.log("APPTS??", date.appointments);
+
+        for (const result of date.appointments) {
+          // console.log(result);
+
+          if (appt.id === result) {
+            apptArr.push(appt);
+
+          }
+        }
+      })
+    }
+  });
+  console.log("FINAL", apptArr);
+  return apptArr;
+
+}
+
+// getAppointmentsForDay(state, "Wednesday");
