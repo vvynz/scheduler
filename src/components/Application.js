72a717ffbd1b172eebd97a857f1f55db1741ff10
Vivian Zhang <vvynz12@gmail.com>
2022-02-25 18:14:02 -0500

    Update Promise to make a 3rd call to /interviewers

--- src/components/Application.js
+++ src/components/Application.js
@@ -13,32 +13,33 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   })
   // const [day, setDay] = useState("Monday");
   // const [days, setDays] = useState([]);
 
-  // Promise.all will make GET requests to /days & /appointments
-  Promise.all([
-    axios.get('/api/days'),
-    axios.get('/api/appointments')
-  ]).then((all) => {
-    setState(prev => ({
-      ...prev,
-      days: all[0].data,
-      appointments: all[1].data
-    }));
-  })
 
   // our array of appointments returned from our helper function
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const appointmentsArr = dailyAppointments.map(appointment => <Appointment key={appointment.id} {...appointment} />)
+  const schedule = dailyAppointments.map(appointment => <Appointment key={appointment.id} {...appointment} />)
 
 
   useEffect(() => {
-    axios.get('/api/days').then(response => {
-      // console.log(response.data);
-      // setDays([...response.data]);
+
+    // Promise.all will make GET requests to /days & /appointments
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      console.log("MEOW", all[2].data);
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }));
     })
   }, [])
 
@@ -62,7 +63,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointmentsArr}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
