4180a10c55b7efda8c72d7c0b4132baa0aea135d
Vivian Zhang <vvynz12@gmail.com>
2022-02-26 16:15:25 -0500

    Refactor and import the getInterview function and have the appointment components display the student name and interviewer name

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment';
-import getAppointmentsForDay from 'helpers/selectors';
+import { getAppointmentsForDay, getInterview } from 'helpers/selectors';
 
 export default function Application(props) {
   const setDay = day => setState({ ...state, day });
@@ -22,7 +22,13 @@ export default function Application(props) {
 
   // our array of appointments returned from our helper function
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const schedule = dailyAppointments.map(appointment => <Appointment key={appointment.id} {...appointment} />)
+  const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={interview} />
+    )
+  })
 
 
   useEffect(() => {
