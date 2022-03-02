34b210a529a0440d56c0e37e0c3e73ac3c30ed4e
Vivian Zhang <vvynz12@gmail.com>
2022-03-02 13:03:16 -0500

    Import cleanup from React

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
 import Appointment from 'components/Appointment';
+
+afterEach(cleanup);
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
