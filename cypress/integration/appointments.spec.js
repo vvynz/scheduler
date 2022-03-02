b45879d06944b57ee72ece8ac94f32ac1d9d9423
Vivian Zhang <vvynz12@gmail.com>
2022-03-02 18:53:25 -0500

    Create a test file for the Appointments

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,23 @@
+describe("Appoinments", () => {
+  beforeEach()
+  it("should book an interview", () => {
+    cy.contains("Monday");
+
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+
+    cy.get("[data-testid=student-name-input]")
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Sylvia Palmer']")
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+
+  });
+});
