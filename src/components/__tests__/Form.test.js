b0fb2f11b1295192d15e641102b089bbb85c0a1a
Vivian Zhang <vvynz12@gmail.com>
2022-03-02 13:35:49 -0500

    Setup first couple of tests properly

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -16,10 +16,12 @@ describe("Form", () => {
   ];
 
   it("renders without student name if not provided", () => {
+    const { getByPlaceholderText } = render(<Form interviewers={interviewers} />);
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
   });
 
   it("renders with initial student name", () => {
+    const { getByTestId } = render(<Form interviewers={interviewers} student="Lydia Miller-Jones" />);
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 });
