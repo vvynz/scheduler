00da95ba326ad765d2390ae16932e4c8dc80cc17
Vivian Zhang <vvynz12@gmail.com>
2022-03-02 18:22:09 -0500

    Update test to check for the class instead of a css background property

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,10 +4,11 @@ describe("Navigation", () => {
   });
 
   it("should navigate to Tuesday", () => {
-    cy.visit("/")
-    cy.contains("li", "Tuesday")
+    cy.visit("/");
+
+    cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should("have.css", "background-color", "rgb(242, 242, 242)");
+      .should("have.class", "day-list__item--selected");
 
   });
 
