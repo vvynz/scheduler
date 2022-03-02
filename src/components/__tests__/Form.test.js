8089fd1f3b785ef04824f7a3432a2ca521f48915
Vivian Zhang <vvynz12@gmail.com>
2022-03-02 13:53:39 -0500

    Import fireEvent

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
@@ -24,4 +24,28 @@ describe("Form", () => {
     const { getByTestId } = render(<Form interviewers={interviewers} student="Lydia Miller-Jones" />);
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
+
+  it("validates that the student name is not blank", () => {
+    /* 1. Create the mock onSave function */
+
+    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
+
+    /* 3. Click the save button */
+
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  });
+
+  it("calls onSave function when the name is defined", () => {
+    /* 1. Create the mock onSave function */
+
+    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
+
+    /* 3. Click the save button */
+
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
+
 });
