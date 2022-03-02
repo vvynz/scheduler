d56140394c5f6e01bfd18583767db68be008c72b
Vivian Zhang <vvynz12@gmail.com>
2022-03-01 21:18:14 -0500

    Import propTypes and check if props passed to InterviewerList is an array

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
+import propTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   // console.log("LIST", props);
@@ -18,6 +19,9 @@ export default function InterviewerList(props) {
     );
   });
 
+  InterviewerList.propTypes = {
+    interviewers: propTypes.array.isRequired
+  };
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
