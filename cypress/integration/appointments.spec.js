0d2b2134256d38f6e8d8e32bd9c2614df15e9c89
Vivian Zhang <vvynz12@gmail.com>
2022-03-02 19:09:50 -0500

    Add test to test editing an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -27,6 +27,21 @@ describe("Appoinments", () => {
   });
 
   it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
 
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
